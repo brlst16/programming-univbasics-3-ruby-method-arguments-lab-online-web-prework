68776530b796313d5575113fbf1de18777fc28dd
Brigette LeVert <bnl.levert@gmail.com>
2019-09-07 20:49:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,11 +4,14 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
- puts "Hello, My name is #{name}"
+ puts "Hi, my name is #{name}"
 end
-introduction("Sam")
+introduction("Josh")
 
-
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+introduction_with_language("Dan", "Ember.js")
 
 
 
